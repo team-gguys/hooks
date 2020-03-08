f2a861355f19076e9c78668c0f0d47aef284a233
Nhua Nhua <nhuan.hoangduc@outlook.com>
2020-03-08 21:39:29 +0700

    Rebuild

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
-export { default as memo } from './hooks/memo'
-export { default as useCallback } from './hooks/useCallback'
-export { default as useEffect } from './hooks/useEffect'
-export { default as useMemo } from './hooks/useMemo'
-export { default as useState } from './hooks/useState'
+import memo from './hooks/memo'
+import useCallback from './hooks/useCallback'
+import useEffect from './hooks/useEffect'
+import useMemo from './hooks/useMemo'
+import useState from './hooks/useState'
 
-export default 'team-gguys-hooks'
+export default {
+    memo: memo,
+    useCallback: useCallback,
+    useEffect: useEffect,
+    useMemo: useMemo,
+    useState: useState,
+}
