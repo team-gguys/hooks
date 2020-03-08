9f3faa8519dae66f3e00ca94316564aa871aebca
Nhua Nhua <nhuan.hoangduc@outlook.com>
2020-03-08 20:54:20 +0700

    Add webpack

--- index.js
+++ index.js
@@ -1,12 +1,5 @@
-import memo from './hooks/memo'
-import useCallback from './hooks/useCallback'
-import useEffect from './hooks/useEffect'
-import useMemo from './hooks/useMemo'
-import useState from './hooks/useState'
-
-
-export const memo = memo
-export const useCallback = useCallback
-export const useEffect = useEffect
-export const useMemo = useMemo
-export const useState = useState
+export { default as memo } from './hooks/memo'
+export { default as useCallback } from './hooks/useCallback'
+export { default as useEffect } from './hooks/useEffect'
+export { default as useMemo } from './hooks/useMemo'
+export { default as useState } from './hooks/useState'
