1382fc2e105647a9ad07b2e9008ebe7ec89c7e01
Nhua Nhua <nhuan.hoangduc@outlook.com>
2020-03-08 21:05:41 +0700

    Rebuild

--- index.js
+++ index.js
@@ -1,5 +1,20 @@
-export { default as memo } from './hooks/memo'
-export { default as useCallback } from './hooks/useCallback'
-export { default as useEffect } from './hooks/useEffect'
-export { default as useMemo } from './hooks/useMemo'
-export { default as useState } from './hooks/useState'
+import customMemo from './hooks/memo'
+import useCustomCallback from './hooks/useCallback'
+import useCustomEffect from './hooks/useEffect'
+import useCustomMemo from './hooks/useMemo'
+import useCustomState from './hooks/useState'
+
+export const memo = customMemo
+export const useCallback = useCustomCallback
+export const useEffect = useCustomEffect
+export const useMemo = useCustomMemo
+export const useState = useCustomState
+
+
+export default {
+    memo: memo,
+    useCallback: useCallback,
+    useEffect: useEffect,
+    useMemo: useMemo,
+    useState: useState,
+}
